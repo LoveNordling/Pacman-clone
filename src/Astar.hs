ebe78f14c9e29095f3e73b288603baf880e64e31
ulfsigvardsson <ulf_sigvardsson@hotmail.com>
2017-02-15 22:30:33 +0000

    Completed neighbours function, added openList and closedList

--- src/Astar.hs
+++ src/Astar.hs
@@ -1,20 +1,65 @@
 import Tile
+import Test.HUnit
+import Data.Maybe
+type Coords = (Int, Int)
+type TileTuple = (Coords, Tile)
 
 {- neighbours (x, y) ts
    POST: All neighbouring tiles for the tile at coordinate (x, y)
+   EXAMPLES: neigbours (1,1) (Tile (True, True, True, False) Void) [(2,1), Tile (True, False, True, True) Void)] = [Just ((Tile (True, False, True, True) Void))]
 -}
-neighbours :: (Int, Int) -> Tile-> [((Int, Int), Tile)] -> [Maybe Tile]
-neighbours (x,y) (Tile (t, l, b ,r) _) ts  =
+neighbours :: Coords -> Tile-> [TileTuple] -> [TileTuple] -> [TileTuple] -> [Tile]
+neighbours (x,y) (Tile (t, l, b ,r) _) board open closed =
   let
-    left   = findNeighbour (x-1, y) ts
-    right  = findNeighbour (x+1, y) ts
-    top    = findNeighbour (x  , y-1) ts
-    bottom = findNeighbour (x  , y+1) ts
+    left   = neighbour l (x-1, y  ) board
+    right  = neighbour r (x+1, y  ) board
+    top    = neighbour t (x  , y-1) board
+    bottom = neighbour b (x  , y+1) board
+    
+    neighbour :: Bool -> Coords -> [TileTuple] -> Maybe Tile
+    neighbour _ _  []  = Nothing -- basfall, tom plan
+    neighbour True _ _ = Nothing -- är boolen true är det en vägg ivägen
+    neighbour b (x, y) (((x',y'), t):xs) 
+      | x==x' && y==y' = Just t  -- koordinaten vi söker
+      | otherwise      = neighbour b (x, y) xs -- annars rekuresera
+      
+    justList = filter isJust [left, right, top, bottom] --filtrerar bort Nothings
   in
-    [left, right, top, bottom]
+    map fromJust justList --Lyfter ur alla "Just v" till "v"
 
-findNeighbour :: (Int, Int) -> [((Int, Int), Tile)] -> Maybe Tile
-findNeighbour _  [] = Nothing
-findNeighbour (x, y) (((x',y'), t):xs)
-  | x==x' && y==y' = Just t
-  | otherwise      = findNeighbour (x, y) xs
+{- filterClosedNeighbours closed ns
+   POST: all elements in ns not present in closed
+-}
+filterClosedNeighbours :: [TileTuple] -> [TileTuple] -> [TileTuple]
+filterClosedNeighbours closed ns = filter (`notElem` closed) ns
+
+{- removeFromOpenList t openlist
+   POST: openlist with t removed
+-}
+removeFromOpenList :: TileTuple -> [TileTuple] -> [TileTuple]
+removeFromOpenList t []     = []
+removeFromOpenList t (x:xs)
+  | t==x                    = xs
+  | otherwise               = x:removeFromOpenList t xs
+
+{- openListPop stack
+   POST: (head of stack, rest of stack)
+-}
+openListPop :: [TileTuple] -> (TileTuple,[TileTuple])
+openListPop [] = error "Empty stack"
+openListPop ts = (head ts, tail ts)
+
+addToClosedList :: TileTuple -> [TileTuple] -> [TileTuple]
+addToClosedList t ts = t:ts
+
+
+
+                 
+test1 = TestCase $ assertEqual ("neighbours")
+        [(Tile (True, False, True, True) Void)]
+        (neighbours (1,1) (Tile (True, True, True, False) Void) [((2,1), Tile (True, False, True, True) Void)] [] [])
+
+test2 = TestCase $ assertEqual ("neighbours")
+        []
+        (neighbours (1,1) (Tile (True, True, True, True) Void) [((2,1), Tile (True, False, True, True) Void)] [] [])
+
