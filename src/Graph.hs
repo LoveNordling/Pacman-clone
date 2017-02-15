2c2f912d0aa16db6c4eb133b42140f6070cd5461
ulfsigvardsson <ulf_sigvardsson@hotmail.com>
2017-02-15 15:17:52 +0000

    Second commit

--- src/Graph.hs
+++ src/Graph.hs
@@ -1,13 +1,9 @@
 import Data.Graph
+data Tile = Tile Vertex (Int, Int) deriving (Show)
+data Map = Map Graph Tile Tile
 
-bounds :: Bounds
-bounds = (1,8)
-
-edge :: [Edge]
-edge = [(1,2),(1,8),(2,1),(3,2),(4,3),(5,4),(5,6),(6,5),(6,7),(7,6),(7,8),(8,7),(8,1)]
-
-playerTile :: Vertex
-playerTile = 5
+build :: (Int,Int) -> [Edge] -> Graph
+build bounds edges = buildG bounds edges
 
 allVertices :: Graph -> [Vertex]
 allVertices g = vertices g
@@ -15,23 +11,16 @@ allVertices g = vertices g
 allEdges :: Graph -> [Edge]
 allEdges g = edges g
 
-computerTile :: Vertex
-computerTile = 1
-
 reachableByComputer :: Graph -> Vertex -> [Vertex]
 reachableByComputer g cTile = reachable g cTile
 
 canPursuit :: Graph -> Vertex -> Vertex -> Bool
 canPursuit g cTile pTile = path g cTile pTile
 
-build :: Graph
-build = buildG bounds edge
+extractVertex :: Tile -> Vertex
+extractVertex (Tile v (a,b)) = v
 
-addToClosedSet :: [Vertex] -> Vertex -> [Vertex]
-addToClosedSet set tile = tile:set
-
-removeFromOpenSet :: [Vertex] -> Vertex -> [Vertex]
-removeFromOpenSet set tile
-  | tile == head set = tail set
-  | otherwise        = head set: removeFromOpenSet (tail set) tile
-  
+connectionsInTile :: Tile -> Graph -> [Edge]
+connectionsInTile (Tile v _)  g = let edges = allEdges g
+                     in
+                       filter (\x-> (snd x)==v) edges
