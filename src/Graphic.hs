51bf9d2c44e1832319c368b3e0cfe363563fa6e5
Ardalan Samimi <ardalan.samimi@gmail.com>
2017-02-15 11:03:27 +0100

    Cleaned out old files

--- src/Graphic.hs
+++ src/Graphic.hs
@@ -1,14 +1,13 @@
-module Graphic(generateWindow) where
-import Board
+module Graphic where
 import Graphics.Gloss
 
 {- window x y
    PRE:           x and y > 0
    POST:          a window with size (x, y)
-   EXAMPLES:      window 1000 1000 gives a window with width and height 1000.
+   EXAMPLES:      ...
 -}
 window :: Int -> Int -> Display
-window x y = InWindow "Dazzlebox" (x, y) (0, 0)
+window x y = undefined
 
 {- background
    PRE:           True
@@ -16,36 +15,21 @@ window x y = InWindow "Dazzlebox" (x, y) (0, 0)
    EXAMPLES:      background ==
 -}
 background :: Color
-background = white
+background = undefined
 
--- 499x355
+{- createBoard
+   PRE:           ...
+   POST:          ...
+   EXAMPLES:      createBoard ==
+   VARIANT:       ...
+-}
+createBoard :: Picture
+createBoard = undefined
 
-board :: Picture
-board = Pictures [
-  Line [
-    (-480, -300), (-480, -250),
-    (-430, -250), (-430, -300),
-    (-480, -300),
-
-    (-480, -250), (-480, -200),
-    (-430, -200), (-430, -250),
-    (-480, -250)
-  ],
-  Line [
-    (-430, -300), (-380, -300),
-    (-380, -250), (-430, -250),
-    (-430, -200),
-
-    (-430, -250), (-380, -250),
-    (-380, -200), (-430, -200),
-    (-430, -250)
-  ],
-
-  Line [(499,-335), (0,-335)]
- ]
-
-createBoard :: [Tile] -> Picture
-createBoard (t:ts) = undefined
-
-generateWindow :: [Tile] -> IO()
-generateWindow tiles = display (window 1000 1000) background board--(createBoard tiles)
+{- generateWindow
+   PRE:           ...
+   POST:          ...
+   EXAMPLES:      generateWindow ==
+-}
+generateWindow :: IO()
+generateWindow = undefined
--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,5 @@
 module Main where
 import Graphic
-import Board
-
-tiles = [
-  Tile (True, True, True, False) Void,
-  Tile (True, True, True, False) Void,
-  Tile (False, False, False, True) Void,
-  Tile (True, False, False, True) Void
-  ]
 
 main :: IO ()
-main = Graphic.generateWindow tiles
+main = undefined
