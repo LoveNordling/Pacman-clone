ce1ef8fcdf35ef1ef1fa31679c9b3c4cf37d6399
ulfsigvardsson <ulf_sigvardsson@hotmail.com>
2017-02-16 10:04:01 +0000

    Updated neighbours function, added distance calculations between current tile and goal.

--- src/Astar.hs
+++ src/Astar.hs
@@ -3,35 +3,71 @@ import Test.HUnit
 import Data.Maybe
 type Coords = (Int, Int)
 type TileTuple = (Coords, Tile)
+type Path' = [Coords]
+
+testMap :: [TileTuple]
+testMap = [((0,0), Tile (True, True, False, False) Void), ((0,1), Tile (True, False, True, True) Void), ((1,0), Tile (False, True, True, True) Void)]
+
+testTile :: TileTuple
+testTile = ((0,0), Tile (True, True, False, False) Void)
+
+findGoal :: TileTuple -> TileTuple  -> [TileTuple] ->  [TileTuple] -> [(Int, TileTuple)]-> Maybe Path'
+findGoal (currentPos, currentTile) (goalPos, goalTile) board closed open
+  | currentPos == goalPos = Just [(0,0)]
+  | otherwise             = undefined
 
 {- neighbours (x, y) ts
    POST: All neighbouring tiles for the tile at coordinate (x, y)
    EXAMPLES: neigbours (1,1) (Tile (True, True, True, False) Void) [(2,1), Tile (True, False, True, True) Void)] = [Just ((Tile (True, False, True, True) Void))]
--}
-neighbours :: Coords -> Tile-> [TileTuple] -> [TileTuple] -> [TileTuple] -> [Tile]
-neighbours (x,y) (Tile (t, l, b ,r) _) board open closed =
+-}                            
+neighbours :: Coords -> Tile -> [TileTuple] -> Coords-> [(Int, TileTuple)]
+neighbours (x,y) t board goalCoords =
+  let
+    ns = neighboursAux (x, y) t board
+    dist = map (distanceToGoal goalCoords) ns
+  in
+    zip dist ns 
+
+neighboursAux :: Coords -> Tile-> [TileTuple] -> [TileTuple]
+neighboursAux (x,y) (Tile (t, l, b ,r) _) board =
   let
     left   = neighbour l (x-1, y  ) board
     right  = neighbour r (x+1, y  ) board
     top    = neighbour t (x  , y-1) board
     bottom = neighbour b (x  , y+1) board
     
-    neighbour :: Bool -> Coords -> [TileTuple] -> Maybe Tile
+    neighbour :: Bool -> Coords -> [TileTuple] -> Maybe TileTuple
     neighbour _ _  []  = Nothing -- basfall, tom plan
     neighbour True _ _ = Nothing -- är boolen true är det en vägg ivägen
     neighbour b (x, y) (((x',y'), t):xs) 
-      | x==x' && y==y' = Just t  -- koordinaten vi söker
+      | x==x' && y==y' = Just ((x, y),t)  -- koordinaten vi söker
       | otherwise      = neighbour b (x, y) xs -- annars rekuresera
-      
-    justList = filter isJust [left, right, top, bottom] --filtrerar bort Nothings
   in
-    map fromJust justList --Lyfter ur alla "Just v" till "v"
+    map fromJust $ filter isJust [left, right, top, bottom]  --filtrerar bort Nothings och lyfter ur alla "Just v" till "v"
+
+
+
+    
+distanceToGoal :: Coords -> TileTuple -> Int
+distanceToGoal (x,y) ((x',y'),_) = (abs (x'-x)) + (abs(y'-y))
 
 {- filterClosedNeighbours closed ns
    POST: all elements in ns not present in closed
 -}
 filterClosedNeighbours :: [TileTuple] -> [TileTuple] -> [TileTuple]
 filterClosedNeighbours closed ns = filter (`notElem` closed) ns
+
+
+{- openListPop stack
+   POST: (head of stack, rest of stack)
+-}
+openListPop :: [TileTuple] -> (TileTuple,[TileTuple])
+openListPop [] = error "Empty stack"
+openListPop [t] = (t, [])
+openListPop ts = (head ts, tail ts)
+
+addToClosedList :: TileTuple -> [TileTuple] -> [TileTuple]
+addToClosedList t ts = t:ts
 
 {- removeFromOpenList t openlist
    POST: openlist with t removed
@@ -42,24 +78,13 @@ removeFromOpenList t (x:xs)
   | t==x                    = xs
   | otherwise               = x:removeFromOpenList t xs
 
-{- openListPop stack
-   POST: (head of stack, rest of stack)
--}
-openListPop :: [TileTuple] -> (TileTuple,[TileTuple])
-openListPop [] = error "Empty stack"
-openListPop ts = (head ts, tail ts)
-
-addToClosedList :: TileTuple -> [TileTuple] -> [TileTuple]
-addToClosedList t ts = t:ts
+addToOpenList ::  [(Int, TileTuple)] -> (Int, TileTuple) -> [(Int, TileTuple)]
+addToOpenList [] (n, t) = [(n,t)]
+addToOpenList ((n',t'):xs) (n, t)
+  | n<n' = (n,t):(n',t'):xs
+  | otherwise = (n',t'):addToOpenList xs (n,t)
+        
 
 
 
-                 
-test1 = TestCase $ assertEqual ("neighbours")
-        [(Tile (True, False, True, True) Void)]
-        (neighbours (1,1) (Tile (True, True, True, False) Void) [((2,1), Tile (True, False, True, True) Void)] [] [])
-
-test2 = TestCase $ assertEqual ("neighbours")
-        []
-        (neighbours (1,1) (Tile (True, True, True, True) Void) [((2,1), Tile (True, False, True, True) Void)] [] [])
 
--- src/Tile.hs
+++ src/Tile.hs
@@ -1,7 +1,33 @@
-module Tile(Actor(..), Tile(..)) where
+module Tile(Actor(..), Tile(..), generateBoard, Position, Board) where
+
+-- Suppress HLint messages
+{-# ANN module "HLint: Ignore Use mappend" #-}
 
 data Actor = Void | Player | Computer deriving (Show, Eq)
-
-
 -- Top, left, bottom, right
 data Tile = Tile (Bool, Bool, Bool, Bool) Actor deriving (Show, Eq)
+
+type Coords    = (Int,Int)
+type Position  = (Float, Float)
+type Board     = [(Position, Tile)]
+
+{-
+	generateBoard matrix
+	PRE:
+	POST: a list of tupels of a tile as snd arg and it's position as its first
+-}
+
+generateBoard :: [[Tile]] -> Board
+generateBoard t@(x:xs) = generateBoardAux t (-(length x)//2) ((length t)//2)
+
+generateBoardAux :: [[Tile]] -> Float -> Float -> Board
+generateBoardAux [] _ _ = []
+generateBoardAux (t:ts) x y = (generateBoardRow t x y) ++ generateBoardAux ts x (y-1)
+
+generateBoardRow :: [Tile] -> Float -> Float -> Board
+generateBoardRow [] _ _ = []
+generateBoardRow (t:ts) x y = ((x,y),t) : (generateBoardRow ts (x+1) y)
+
+
+(//) :: Int -> Int -> Float
+(//) a b = fromIntegral (div a b)
