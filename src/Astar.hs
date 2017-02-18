db818c0980145da8c0b9982720d89840ab7e2458
ulfsigvardsson <ulf_sigvardsson@hotmail.com>
2017-02-18 13:22:55 +0000

    Fixat Astar, behöver commita denna för att importera Tile från master

--- src/Astar.hs
+++ src/Astar.hs
@@ -1,71 +1,131 @@
+import Data.List
+import qualified Test.HUnit as T
 import Tile
-import Debug.Trace
-import Data.Maybe
 
-
-type ValidPathsFunc = (Grid -> Position -> Path)
-type CostFunc       = (Position -> Path -> Float)
-type Grid = [[Tile]]
+type Board = [Tile]
+type Position = (Float, Float)
 type Path = [Position]
+type PossiblePath = (Int, Path)
+
 {-
-  F F W F
-  F W W F
-  F F F F
-  W W W W-}
-testBoard :: Grid
-testBoard = [[Tile (0.0,0.0) Floor Void, Tile (0.0,1.0) Floor Void, Tile (0.0,2.0) Wall Void, Tile (0.0,3.0) Floor Void],
-             [Tile (1.0,0.0) Floor Void, Tile (1.0,1.0) Wall Void, Tile (1.0,2.0) Wall Void,  Tile (1.0,3.0) Floor Void],
-             [Tile (2.0,0.0) Floor Void, Tile (2.0,1.0) Floor Void, Tile (2.0,2.0) Floor Void, Tile (2.0,3.0) Floor Void],
-             [Tile (3.0,0.0) Wall Void, Tile (3.0,1.0) Wall Void, Tile (3.0,2.0) Wall Void, Tile (3.0,3.0) Wall Void]]
-
-
-{- aStar grid goal pos vpf cf
-   PRE: grid contains a Tile with coordinates goal and coordinates pos
-   POST: 
-   SIDE EFFECTS: none
-   EXAMPLES:
+ F W W F F
+ F W W F W
+ F F F F W
+ W F W F F
+ F F W W W
 -}
-aStar :: Grid -> Position -> Position -> ValidPathsFunc -> CostFunc -> Path
-aStar grid goal pos vpf cf = head $ aStarAux grid goal vpf cf [[pos]]
 
-aStarAux :: Grid -> Position -> ValidPathsFunc -> CostFunc -> [Path] -> [Path]
-aStarAux grid goal vpf cf paths
-  | any (\p -> last p == goal) paths = filter (\p -> last p == goal) paths
-  | otherwise                        = let
-                                         best = snd $ minimum $ zip (map (cf goal) paths) paths
-                                         pb = addRoutes grid paths best vpf
-                                       in
-                                         aStarAux grid goal vpf cf $ filter (/= best) paths ++ pb
 
-addRoutes :: Grid -> [Path] -> Path -> ValidPathsFunc -> [Path]
-addRoutes grid paths path vpf = let
-                               cpaths = concat paths
-                             in
-                               [ path ++ [p] | p <- filter (`notElem` cpaths) $ vpf grid $ last path]
 
-    
-cost :: Position -> Path -> Float
-cost goal path = let
-                   current = last path
-                   tCost = fromIntegral (length path -1) :: Float
-                 in
-                   tCost + (distance current goal)
+board :: Board
+board = [Floor (-2,-2), Wall (-1,-2), Wall (0,-2), Floor (1,-2), Floor (2,-2),
+         Floor (-2,-1), Wall (-1,-1), Wall (0,-1), Floor (1,-1), Wall (2,-1),
+         Floor (-2,0), Floor (-1,0), Floor (0,0), Floor (1,0), Wall (2,0),
+         Wall (-2,1), Floor (-1,1), Wall (0,1), Floor (1,1), Floor (2,1),
+         Floor (-2,2), Floor (-1,2), Wall (0,2), Wall (1,2), Wall (2,2) ]
+
+findNeighbour (x, y) =
+  [ (a, b) | a <- [(x-1)..(x+1)], b <- [(y-1)..(y+1)], satisfy (x, y) (a,b) ]
+
+satisfy :: Position -> Position -> Bool
+satisfy (a, b) (c, d) =
+  c > (-3) && c < 3 && d > (-3) && d < 3 && not ((c==a && d == b)) &&(c-a == 0 || d-b ==0)
+
+
+newPaths :: Position -> Board -> [(Float, Float)]
+newPaths current p =
+  let
+    n  = findNeighbour current
+    n' = filter (isValidMove p) n
+  in
+    n'
 
 distance :: Position -> Position -> Float
 distance (x0, y0) (x1, y1) = abs (x1-x0) + abs (y1-y0)
 
+cost :: Position -> Path -> Float
+cost goal path = let
+                   current = last path
+                   steps   = fromIntegral (length path -1) :: Float
+                 in
+                   steps + (distance current goal)
 
-validPaths :: Grid -> Position -> Path
-validPaths grid (x, y) = [(x1, y1) | x1 <- [(x-1)..(x+1)],
-                                     y1 <- [(y-1)..(y+1)],
-                                     x1 >= 0,
-                                     y1 >= 0,
-                                     x1 <  (fromIntegral (length grid) :: Float),
-                                     y1 <  (fromIntegral (length grid) :: Float),
-                                     isNotWall (grid !! round x1 !! round y1),
-                                     x-x1 ==0 || y-y1==0
-                                     ]
+{- pathLists board path
+   PRE:
+   POST: list of all valid neighbours of the last element in path appended to path 
+-}
+pathLists ::  Board -> Path -> [Path]
+pathLists board path =
+  let
+    paths    = filter (`notElem` path) $ newPaths (last path) board
+    pathList = map (\p->path++[p]) paths
+  in
+    pathList
 
-isNotWall :: Tile -> Bool
-isNotWall (Tile _ Floor _) = True
-isNotWall (Tile _ Wall _)  = False
+{- isValidMove board pos
+   PRE:
+   POST: True if and only if 
+   SIDE EFFECTS:
+   VARIANT:
+   EXAMPLES:
+-}
+isValidMove :: Board -> Position -> Bool
+isValidMove [] _       = False
+isValidMove ((Wall (x,y)):bs) (x0, y0)
+  | x == x0 && y == y0 = False
+  | otherwise          = isValidMove bs (x0,y0)
+isValidMove ((Floor (x,y)):bs) (x0,y0)
+  | x == x0 && y == y0 = True
+  | otherwise          = isValidMove bs (x0, y0)
+
+{- costOfPaths paths goal
+   PRE:
+   POST: list of every path in paths mapped to the projected cost of the last tile in it
+-}
+costOfPaths :: [Path] -> Position -> [(Float, Path)]
+costOfPaths paths goal = let
+                           costs = map (cost goal) paths
+                         in
+                           zip costs paths
+
+aStar :: Board -> Position -> Position -> [Path]
+aStar board goal start = aStarAux board goal [[start]]
+  
+aStarAux :: Board -> Position -> [Path] -> [Path]
+aStarAux board goal paths
+  | any (\p -> last p == goal) paths = filter (\p-> last p == goal) paths
+  | otherwise =
+    let
+      best = snd $ minimum $ costOfPaths paths goal
+      next = pathLists board best
+    in
+      aStarAux board goal $ filter (/= best) paths ++ next
+      
+
+test1 = let
+          goal = (2,1)
+          start = (-2,-2)
+        in
+          T.TestCase $ T.assertEqual "aStar"
+          ([[(-2.0,-2.0),(-2.0,-1.0),(-2.0,0.0),(-1.0,0.0),(0.0,0.0),(1.0,0.0),(1.0,1.0),(2.0,1.0)]])
+          (aStar board goal start)
+
+test2 = let
+          goal = (-2,2)
+          start = (2,-2)
+        in
+          T.TestCase $ T.assertEqual "aStar"
+          ([[(2.0,-2.0),(1.0,-2.0),(1.0,-1.0),(1.0,0.0),(0.0,0.0),(-1.0,0.0),(-1.0,1.0),(-1.0,2.0),(-2.0,2.0)]])
+          (aStar board goal start)
+
+
+
+
+
+
+
+
+
+
+
+    
